a41028b00bfe7425e138f25752fc5df183211aff
Tim Hulak <timhulak@gmail.com>
2019-04-17 19:42:03 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#Database Name: twitch_db
+#localhost:5000
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/twitch_data')
+def twitch_data():
+    connection_string = 'mysql+pymysql://root:1065Delta@localhost/twitch_db'
+    engine = create_engine(connection_string)
+
+    df = pd.read_sql('select * from twitch_overall',engine)
+
+    data = {}
+    for i in df.columns:
+        data[i] = df.loc[i].values.tolist()
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)

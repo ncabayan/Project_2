a9864511a6c8bef2390fa73be6ee1563fff1e769
cssource123 <43654896+cssource123@users.noreply.github.com>
2019-04-18 11:49:45 -0700

    finished

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,25 @@
 var defaultURL = "/twitch_data";
-d3.json(defaultURL).then(function(data){
-	var data= [data];
-	var layout = {margin: { t: 30, b: 100}};
-	Plotly.plot('scatter', data, layout);
-});
-function updatePlotly(newdata){
-	Plotly.restyle('scatter', 'x', [newdata.x]);
-	Plotly.restyle('scatter', 'y', [newdata.y]);
-}
+function optionChanged(selection){    
+    d3.json(defaultURL).then(function(data){
+        console.log(data);
+        var filter = data.filter(d => d.player_name == selection);
+        console.log(filter);
+        //var data = [data];
+        //var layout = {margin: { t: 30, b: 100}};
+        var trace1 = {
+            x: filter.map(d => d.follower_ping),
+            y: filter.map(d => d.follower_count),
+            mode: 'lines',
+            type: 'scatter'
+        }
+        var filterdata = [trace1]
+        Plotly.plot('streamer', filterdata);
+})}
 
 function getData(route){
-	console.log(route);
-	d3.json(`/${route}`).then(function(data){
-		console.log("newdata", data);
-		updatePlotly(data);
-	});
+    console.log(route);
+    d3.json(`/${route}`).then(function(data){
+        console.log("newdata", data);
+        updatePlotly(data);
+    });
 }
